0aa3a9a3b754d261d303999ff4f3b193a3535a09
galsavu <galsavu@gmail.com>
2022-01-19 15:52:02 +0200

    added address dto to server

--- PingPong/Server/PingPongServer.cs
+++ PingPong/Server/PingPongServer.cs
@@ -1,9 +1,16 @@
 ﻿using Server.Abstraction;
-
+using Common.DTOS;
 namespace Server
 {
     public class PingPongServer : INetworkServer
     {
+        private AddressDTO _address;
+
+        public PingPongServer(AddressDTO address)
+        {
+            _address = address;
+        }
+
         public void Communicate()
         {
             throw new NotImplementedException();
