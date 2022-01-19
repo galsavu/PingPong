118b0f0598022496d9d8334e262b8f123aa84aba
galsavu <galsavu@gmail.com>
2022-01-19 17:04:50 +0200

    client connecting server socket

---
+++ PingPong/Client/PingPongClient.cs
@@ -0,0 +1,28 @@
+﻿using Client.Abstraction;
+using System.Net.Sockets;
+using System.Net;
+
+namespace Client
+{
+    public class PingPongClient : INetworkClient
+    {
+        public IPEndPoint ServerIpEndPoint { get; set; }
+        public Socket ClientSocket { get; set; }
+
+        public PingPongClient(IPAddress ipAddress, int port)
+        {
+            ServerIpEndPoint = new IPEndPoint(ipAddress, port);
+            ClientSocket = new Socket(ServerIpEndPoint.AddressFamily, SocketType.Stream, ProtocolType.Tcp);
+        }
+
+        public void StartSocket()
+        {
+            ClientSocket.Connect(ServerIpEndPoint);
+        }
+
+        public async void Communicate()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
