79edbd3f86910e4d1be5d1615006b11358ac633e
galsavu <galsavu@gmail.com>
2022-01-19 17:57:08 +0200

    added server receive and send

--- PingPong/Server/PingPongServer.cs
+++ PingPong/Server/PingPongServer.cs
@@ -1,12 +1,13 @@
 ﻿using Server.Abstraction;
-using Common.DTOS;
 using System.Net.Sockets;
 using System.Net;
-
+using System.Text;
+using UI.Output.Abstraction;
 namespace Server
 {
     public class PingPongServer : INetworkServer
     {
+        private IOutput output;
         public IPEndPoint ipEndPoint { get; set; }
         public Socket Listener { get; set; }
 
@@ -29,7 +30,11 @@ namespace Server
 
         public async void Communicate()
         {
-            throw new NotImplementedException();
+            byte[] buffer = new byte[1024];
+            int bytesRead = Listener.Receive(buffer);
+            string data = Encoding.ASCII.GetString(buffer, 0, bytesRead);
+            output.Show(data);
+            Listener.Send(Encoding.ASCII.GetBytes(data));
         }
     }
 }
