10bb622cf435aad2125d2e19a266e74091713bda
galsavu <galsavu@gmail.com>
2022-01-19 15:44:00 +0200

    added console output

--- PingPong/UI/Output/ConsoleOutput.cs
+++ PingPong/UI/Output/ConsoleOutput.cs
@@ -3,10 +3,15 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using UI.Output.Abstraction;
 
 namespace UI.Output
 {
-    public class ConsoleOutput
+    public class ConsoleOutput<T> : IOutput<T>
     {
+        public void Show(T obj)
+        {
+            Console.WriteLine(obj);
+        }
     }
 }
