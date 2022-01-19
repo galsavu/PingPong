5ad46714ed37eadf2174352769e6ee4b23be32b1
galsavu <galsavu@gmail.com>
2022-01-19 17:41:50 +0200

    added serialize object

---
+++ PingPong/Common/Encoder/Serialize.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization.Formatters.Binary;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Common.Encoder
+{
+    internal class Serialize
+    {
+        public byte[] SerializeObject(object obj)
+        {
+            BinaryFormatter bin = new BinaryFormatter();
+            MemoryStream mem = new MemoryStream();
+            bin.Serialize(mem, this);
+            return mem.GetBuffer();
+        }
+    }
+}
