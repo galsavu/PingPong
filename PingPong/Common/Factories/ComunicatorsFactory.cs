53f6de82ffc9e3203e673b53c244943d309493ad
galsavu <galsavu@gmail.com>
2022-01-20 12:36:19 +0200

    created socket server communicator

--- PingPong/Common/Factories/ComunicatorsFactory.cs
+++ PingPong/Common/Factories/ComunicatorsFactory.cs
@@ -5,15 +5,34 @@ using System.Text;
 using System.Threading.Tasks;
 using Common.Abstraction;
 using Common.Communicators;
+using UI.Output;
+using UI.Input;
+using UI.Output.Abstraction;
+using UI.Input.Abstraction;
+using Common.Starters;
 namespace Common.Factories
 {
     public class ComunicatorsFactory
     {
+        private readonly OutputFactory _outputFactory;
+        private readonly InputFactory _inputFactory;
 
+        public ComunicatorsFactory()
+        {
+            _outputFactory = new OutputFactory();
+            _inputFactory = new InputFactory();
+        }
 
         public ICommunicator CreateComunicators(string communicatorType, ICommunicateStarter starter, string outputType, string inputType)
         {
-            throw new NotImplementedException();
+            if(communicatorType == "socket server")
+            {
+                IOutput output = _outputFactory.CreateOutput(outputType);
+                IInput input = _inputFactory.CreateInput(inputType);
+                SocketServerStarter socketServerStarter = (SocketServerStarter)starter ;
+                return new SocketServerCommunicator(socketServerStarter, output, input);
+            }
+            return null;
         }
     }
 }
