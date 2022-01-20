f2b9d8030d3647eaa939aa8c6710f78679266b23
galsavu <galsavu@gmail.com>
2022-01-20 12:55:53 +0200

    added socket client starter

---
+++ PingPong/Common/Starters/SockketClientStarter.cs
@@ -0,0 +1,27 @@
+﻿using Common.Abstraction;
+using System.Net;
+using System.Net.Sockets;
+
+namespace Common.Starters
+{
+    public class SockketClientStarter : ICommunicateStarter
+    {
+        public ISender Sender { get; set; }
+        public IReceiver Receiver { get; set; }
+        public Socket ClientSocket { get; set; }
+        public IPEndPoint IpEndPoint { get; }
+
+        public SockketClientStarter(ISender sender, IReceiver receiver, Socket clientSocket, IPEndPoint ipEndPoint)
+        {
+            Sender = sender;
+            Receiver = receiver;
+            ClientSocket = clientSocket;
+            IpEndPoint = ipEndPoint;
+        }
+
+        public void Start(Action communicate)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
