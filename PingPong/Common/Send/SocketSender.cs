86f9198b914d95f868170afb898e5629cae41dda
galsavu <galsavu@gmail.com>
2022-01-20 09:02:31 +0200

    added socket sender

---
+++ PingPong/Common/Send/SocketSender.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Common.Send.Abstraction;
+using System.Net;
+using System.Net.Sockets;
+
+namespace Common.Send
+{
+    public class SocketSender : ISender
+    {
+
+        public void Send(IDisposable sender, object message)
+        {
+            Socket socket = sender as Socket;
+            string messageStr = message.ToString();
+            socket.Send(Encoding.ASCII.GetBytes(messageStr));
+        }
+    }
+}
