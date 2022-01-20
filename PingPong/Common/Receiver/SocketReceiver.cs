3bcd61e3de7818e7aa941d4ec8cb293365469980
galsavu <galsavu@gmail.com>
2022-01-20 09:12:42 +0200

    added socket receiver

---
+++ PingPong/Common/Receiver/SocketReceiver.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Common.Receiver.Abstraction;
+using System.Net;
+using System.Net.Sockets;
+namespace Common.Receiver
+{
+    public class SocketReceiver:IReceiver
+    {
+        private readonly byte[] _buffer;
+
+        public SocketReceiver(byte[] buffer)
+        {
+            _buffer = buffer;
+        }
+
+        public object Receive(IDisposable receiver)
+        {
+            Socket socket = receiver as Socket;
+            int bytesReceived = socket.Receive(_buffer);
+            string data = Encoding.ASCII.GetString(_buffer, 0, bytesReceived);
+            return data;
+        }
+    }
+}
