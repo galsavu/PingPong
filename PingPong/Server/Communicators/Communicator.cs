42f357b19886a204c5c8a71ecdc4ad797732318d
galsavu <galsavu@gmail.com>
2022-01-20 09:53:28 +0200

    added communicator

---
+++ PingPong/Server/Communicators/Communicator.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Server.Abstraction;
+using Common.Receiver.Abstraction;
+using Common.Send.Abstraction;
+
+namespace Server.Communicators
+{
+    public class SocketCommunicator : ICommunicator
+    {
+        private ISender _sender;
+        private IReceiver _receiver;
+        private IDisposable _socket;
+
+        public SocketCommunicator(ISender sender, IReceiver receiver, IDisposable socket)
+        {
+            _sender = sender;
+            _receiver = receiver;
+            _socket = socket;
+        }
+
+        public void communicate()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
