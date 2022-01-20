f18aa27b570e569b21f42d452487348d51306601
galsavu <galsavu@gmail.com>
2022-01-20 11:45:30 +0200

    created sender factory

--- PingPong/Common/Factories/SenderFactory.cs
+++ PingPong/Common/Factories/SenderFactory.cs
@@ -3,10 +3,15 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Common.Abstraction;
 
 namespace Common.Factories
 {
-    internal class SenderFactory
+    public class SenderFactory
     {
+        public ISender CreateSender(string senderType)
+        {
+            throw new NotImplementedException();
+        } 
     }
 }
