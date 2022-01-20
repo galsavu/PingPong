3f60e3a984027e4879832f10e5b2aa9f0b7d938d
galsavu <galsavu@gmail.com>
2022-01-20 14:50:17 +0200

    added client bootstrapper

---
+++ PingPong/Client/Bootstrapper.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Common.Factories;
+using Client.Abstraction;
+
+namespace Client
+{
+    public class Bootstrapper
+    {
+        public void Run(string[] args)
+        {
+            string communicatorType = "socket client";
+            string ipAddress = args[0];
+            int port;
+            int.TryParse(args[1], out port);
+            string outputType = "console";
+            string inputType = "console";
+            var starterFactory = new StarterFactory();
+            var communicatorFactory = new CommunicatorsFactory();
+
+            var server = new PingPongClient(communicatorType, ipAddress, port, outputType, inputType, starterFactory, communicatorFactory);
+
+            server.Start();
+        }
+    }
+}
