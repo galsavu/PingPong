6123ee6c67c7f7b20f604b9ae672a36cd4c1d97b
galsavu <galsavu@gmail.com>
2022-01-20 13:03:10 +0200

    moved server bind to start method

--- PingPong/Common/Starters/SocketServerStarter.cs
+++ PingPong/Common/Starters/SocketServerStarter.cs
@@ -14,16 +14,19 @@ namespace Common.Starters
         public ISender Sender { get; set; }
         public IReceiver Receiver { get; set; }
         public Socket Listener { get; set; }
+        public IPEndPoint IpEndPoint { get; }
 
-        public SocketServerStarter(ISender sender, IReceiver receiver, Socket listener)
+        public SocketServerStarter(ISender sender, IReceiver receiver, Socket listener, IPEndPoint ipEndPoint)
         {
             Sender = sender;
             Receiver = receiver;
             Listener = listener;
+            IpEndPoint = ipEndPoint;
         }
 
         public void Start(Action communicate)
         {
+            Listener.Bind(IpEndPoint);
             Listener.Listen();
             GetClientsConnections(communicate);   
            
