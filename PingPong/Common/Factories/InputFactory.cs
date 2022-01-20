41756e16b9581c176d67e11a6c1fdbe57e02b6d1
galsavu <galsavu@gmail.com>
2022-01-20 12:20:14 +0200

    added input factory

---
+++ PingPong/Common/Factories/InputFactory.cs
@@ -0,0 +1,17 @@
+﻿using UI.Input;
+using UI.Input.Abstraction;
+
+namespace Common.Factories
+{
+    public class InputFactory
+    {
+        public IInput CreateInput(string inputType)
+        {
+            if (inputType == "console")
+            {
+                return new ConsoleInput();
+            }
+            return null;
+        }
+    }
+}
